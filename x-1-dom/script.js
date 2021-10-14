d255a713d150ff8adacd72e4497eb15270239b35
Daniel Nogueira <danielsilvangr@gmail.com>
2021-10-14 19:53:30 -0300

    Create files

---
+++ x-1-dom/script.js
@@ -0,0 +1,5 @@
+function obter(){
+    document.getElementById("p1").innerHTML = "Magic!";
+    document.getElementById("p2").innerHTML = "Magic!";
+    document.getElementById("p3").innerHTML = "Magic!";
+}
